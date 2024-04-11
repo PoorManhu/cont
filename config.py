1831d37dedc2dbf5750f8f3ad30605428bbd7288
PoorManhu <158797332+PoorManhu@users.noreply.github.com>
2024-04-11 17:23:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,9 +31,9 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = "mongodb+srv://Shubh:Shubh@cluster0.thwzwns.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "moneykamalo.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "f2fe103b02077d3fb536578b9dcb27a68c1fec25")
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 604800)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID","SHORTNER_TUTORIAL")
 
